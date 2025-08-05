924870b16377993af62fe7498c88bd438779bf64
leonelvalentinn <leonelvalentinn@gmail.com>
2025-08-05 16:35:24 -0600

    add configuration files for Prettier and ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   pluginReact.configs.flat.recommended,
-  { 
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
     rules: {
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-      "react/react-in-jsx-scope": "off",
-      "semi": "off"
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      semi: 'off',
     },
   },
-]);
+])
