4f9dd9efc2574f0295412442aed5d704c522c51f
leonelvalentinn <leonelvalentinn@gmail.com>
2025-08-05 16:36:40 -0600

    add providers component

--- src/providers.jsx
+++ src/providers.jsx
@@ -1,14 +1,13 @@
-import {HeroUIProvider} from '@heroui/react'
+import { HeroUIProvider } from '@heroui/react'
 import { Layout } from './layout'
+import CartProvider from './context/CartContext'
 
 export const Providers = ({ children }) => {
   return (
-    <>
+    <CartProvider>
       <HeroUIProvider>
-        <Layout>
-          {children}
-        </Layout>
+        <Layout>{children}</Layout>
       </HeroUIProvider>
-    </>
+    </CartProvider>
   )
 }
